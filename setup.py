31ec308c885d16b0274c3503a3e6d8c831e6f72e
marcdelabarrera <mbarrera@mit.edu>
2022-08-25 09:05:01 +0200

    add long description

--- setup.py
+++ setup.py
@@ -6,12 +6,14 @@ with open("README.md","r") as fh:
 
 setup(
     name = 'dsolve',
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'Solver of dynamic equations with forward looking variables',
-    #long_description_content_type='text/markdown',
-    long_description='test',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
     py_modules = ["dsolve.atoms", "dsolve.expressions", "dsolve.solvers"],
     package_dir={'':'src'},
+    author='Marc de la Barrera i Bardalet',
+    author_email='mbarrera@mit.edu',
     install_requires = ["scipy >= 1.9.0"],
     extras_require={"dev":["pytest>=7.1.2",],},
     classifiers =[
